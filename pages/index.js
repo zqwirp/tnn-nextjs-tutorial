7cc005a99b207893b6b1e68dddde3dfa6a14e08a
wijipermana <wiji.permana@gmail.com>
2021-11-26 16:46:43 +0700

    lesson 9, images and metadata

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-// import Head from "next/head"
+import Head from "next/head"
 import styles from "../styles/Home.module.css"
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
       <h1 className={styles.title}>Homepage</h1>
       <p className={styles.text}>Amet culpa similique quo a recusandae. Unde eligendi minus aliquam aliquam nobis recusandae. Sunt alias similique fugit quae illo eius? Voluptate tempora ipsa similique iure saepe Quam minima amet eos.</p>
       <Link href="/ninjas/">
