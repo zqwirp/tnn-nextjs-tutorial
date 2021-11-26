a3c84d2a7bc7ae607c794d0eb4c87475f42d4a55
wijipermana <wiji.permana@gmail.com>
2021-11-26 16:23:27 +0700

    lesson 8, redirecting users

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Oooops...</h1>
