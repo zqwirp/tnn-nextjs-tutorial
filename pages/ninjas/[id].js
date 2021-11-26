6b4c23e4ec952adc6acca67abd96af9114349d11
wijipermana <wiji.permana@gmail.com>
2021-11-26 20:28:01 +0700

    lesson 13, fetching a single item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -14,10 +14,23 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Details() {
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await response.json();
+
+  return {
+    props: { ninja: data },
+  };
+}
+
+export default function Details({ ninja }) {
   return (
     <>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </>
   );
 }
