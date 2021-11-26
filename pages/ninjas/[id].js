0ff181a06ee49b7a7819b282fa1acabc0dcd8861
wijipermana <wiji.permana@gmail.com>
2021-11-26 20:21:25 +0700

    lesson 12, dynamic routes (pt. 2)

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,19 @@
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
 export default function Details() {
   return (
     <>
