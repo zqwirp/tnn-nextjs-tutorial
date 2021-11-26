5699ab53c5ac8eee0ef4ae615123586eeb9b440d
wijipermana <wiji.permana@gmail.com>
2021-11-26 17:04:10 +0700

    lesson 10, fetching data (getStaticProps)

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,25 @@
-export default function Ninjas() {
+import styles from "../../styles/Ninjas.module.css";
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return {
+    props: { ninjas: data }, // will be passed to the page component as props
+  };
+}
+
+export default function Ninjas({ ninjas }) {
   return (
     <>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </>
   );
 }
