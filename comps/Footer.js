9563220d2184c0ba803512cc4321f58af28dd8d4
wijipermana <wiji.permana@gmail.com>
2021-11-26 10:42:18 +0700

    lesson 3, adding other components

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p>&copy; Copyright 2021 Ninja Lists</p>
+      </footer>
+    </>
+  );
+}
---
+++ comps/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Ninja List</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Ninja Listing</a>
+    </nav>
+  );
+}
