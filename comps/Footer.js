e68800fac2a99c8a999bd3d37731fca86278047c
wijipermana <wiji.permana@gmail.com>
2021-11-26 13:45:41 +0700

    lesson 6, adding styles

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,9 +1,7 @@
 export default function Footer() {
   return (
-    <>
-      <footer>
-        <p>&copy; Copyright 2021 Ninja Lists</p>
-      </footer>
-    </>
+    <footer>
+      <p>&copy; Copyright 2021 Ninja Lists</p>
+    </footer>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 // import Head from "next/head"
-// import styles from "../styles/Home.module.css"
+import styles from "../styles/Home.module.css"
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>Homepahe</h1>
+      <h1 className={styles.title}>Homepahe</h1>
+      <p className={styles.text}>Consectetur soluta repellat eius reprehenderit consectetur Nam quidem laboriosam cumque iusto facilis! Architecto quis eaque neque molestias officia? Similique dolorum sed ex voluptas enim? Eius alias quaerat earum dolorem dolores.</p>
       <Link href="/ninjas/">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </>
   );
