df0d0bd1056a508096eafabea390481f7fbba050
wijipermana <wiji.permana@gmail.com>
2021-11-26 20:11:27 +0700

    lesson 11, dynamic routes (pt. 1)

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,4 +1,5 @@
 import styles from "../../styles/Ninjas.module.css";
+import Link from "next/link";
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -14,11 +15,11 @@ export default function Ninjas({ ninjas }) {
     <>
       <h1>All Ninjas</h1>
       {ninjas.map((ninja) => (
-        <div key={ninja.id}>
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </>
   );
