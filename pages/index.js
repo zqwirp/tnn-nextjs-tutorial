c123ffb4e70dd2f4460e7fe7a7934d6e8f1719d1
wijipermana <wiji.permana@gmail.com>
2021-11-26 13:38:14 +0700

    lesson 5, creating a layout component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 // import Head from "next/head"
 // import styles from "../styles/Home.module.css"
 import Link from "next/link";
-import Navbar from "../comps/Navbar"
-import Footer from "../comps/Footer"
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <h1>Homepahe</h1>
       <Link href="/ninjas/">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </>
   );
 }
