8ff12881f735c1c6e1509405864d8a0c46da1e44
wijipermana <wiji.permana@gmail.com>
2021-11-26 11:14:26 +0700

    lesson 4, linking between pages

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav>
       <div className="logo">
         <h1>Ninja List</h1>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Ninja Listing</a>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/ninjas/">Ninja Listing</Link>
     </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 // import Head from "next/head"
 // import styles from "../styles/Home.module.css"
+import Link from "next/link";
 import Navbar from "../comps/Navbar"
 import Footer from "../comps/Footer"
 
@@ -7,7 +8,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
-        <h1>Homepahe</h1>
+      <h1>Homepahe</h1>
+      <Link href="/ninjas/">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </>
   );
