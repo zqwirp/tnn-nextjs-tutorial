dd61566f28dd6151ab5c63c5ab32b7e3db473196
wijipermana <wiji.permana@gmail.com>
2021-11-26 10:31:24 +0700

    lesson 2, pages and routes

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+export default function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Lorem nisi magnam commodi dignissimos ullam? Ea ratione error doloribus animi tempora
+        dolorum Est molestias commodi eos voluptatum perferendis ad vero. Voluptatibus veritatis
+        suscipit quo quasi asperiores Nesciunt voluptas rem.
+      </p>
+      <p>
+        Lorem nisi magnam commodi dignissimos ullam? Ea ratione error doloribus animi tempora
+        dolorum Est molestias commodi eos voluptatum perferendis ad vero. Voluptatibus veritatis
+        suscipit quo quasi asperiores Nesciunt voluptas rem.
+      </p>
+      <p>
+        Lorem nisi magnam commodi dignissimos ullam? Ea ratione error doloribus animi tempora
+        dolorum Est molestias commodi eos voluptatum perferendis ad vero. Voluptatibus veritatis
+        suscipit quo quasi asperiores Nesciunt voluptas rem.
+      </p>
+    </>
+  );
+}
---
+++ pages/ninjas/index.js
@@ -0,0 +1,7 @@
+export default function Ninjas() {
+  return (
+    <>
+      <h1>All Ninjas</h1>
+    </>
+  );
+}
